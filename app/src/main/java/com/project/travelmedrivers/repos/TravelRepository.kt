564cacdcd6ead84014a2688d8c588705fba082d7
mendi160 <Meni Kuperman@DESKTOP-OERQ00U>
2020-12-25 03:03:59 +0200

    TravelRepository

--- app/src/main/java/com/project/travelmedrivers/repos/TravelRepository.kt
+++ app/src/main/java/com/project/travelmedrivers/repos/TravelRepository.kt
@@ -1,19 +1,40 @@
 package com.project.travelmedrivers.repos
 
 import android.app.Application
+import android.content.Context
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
 import com.project.travelmedrivers.data.HistoryDataSource
 import com.project.travelmedrivers.data.IHistoryDataSource
 import com.project.travelmedrivers.data.ITravelDataSource
 import com.project.travelmedrivers.data.ITravelDataSource.NotifyToTravelListListener
 import com.project.travelmedrivers.data.TravelDataSource
 import com.project.travelmedrivers.entities.Travel
+import com.project.travelmedrivers.utils.AddressTool
+import com.project.travelmedrivers.utils.Status
 
 
 class TravelRepository private constructor(application: Application) : ITravelRepository {
     var travelDataSource: ITravelDataSource = TravelDataSource.getInstance()!!
     private val historyDataSource: IHistoryDataSource
     private val mutableLiveData = MutableLiveData<List<Travel?>?>()
+    private var userTravels: MutableList<Travel> = mutableListOf()
+
+    init {
+        historyDataSource = HistoryDataSource(application.applicationContext)
+        val notifyToTravelListListener: NotifyToTravelListListener =
+            object : NotifyToTravelListListener {
+                override fun onTravelsChanged() {
+                    val travelList: List<Travel?> = travelDataSource.getAllTravels()
+                    mutableLiveData.value = travelList
+                    historyDataSource.clearTable()
+                    historyDataSource.addTravels(travelList as List<Travel>)
+                    userTravels()
+                }
+            }
+        travelDataSource.setNotifyToTravelListListener(notifyToTravelListListener)
+    }
+
     override fun addTravel(travel: Travel?) {
         travelDataSource.addTravel(travel!!)
     }
@@ -38,17 +59,41 @@ class TravelRepository private constructor(application: Application) : ITravelRe
         }
     }
 
-    init {
-        historyDataSource = HistoryDataSource(application.applicationContext)
-        val notifyToTravelListListener: NotifyToTravelListListener =
-            object : NotifyToTravelListListener {
-                override fun onTravelsChanged() {
-                    val travelList: List<Travel?> = travelDataSource.getAllTravels()
-                    mutableLiveData.value = travelList
-                    historyDataSource.clearTable()
-                    historyDataSource.addTravels(travelList as List<Travel>)
+    fun userTravels() {
+        val userID = FirebaseAuth.getInstance().currentUser?.email
+        if (userID != null) {
+            userTravels =
+                mutableLiveData.value?.filter { it -> it?.email == userID && it.status != Status.CLOSED } as MutableList<Travel>
+        }
+    }
+
+    fun confirmCompany(travel: Travel, companyEmail: String) {
+        travel.serviceProvider[companyEmail] to true;
+        travelDataSource.updateTravel(travel)
+    }
+
+    fun changeStatus(travel: Travel) {
+        travel.status = Status.RECEIVED
+        travelDataSource.updateTravel(travel)
+    }
+
+    fun relevantTravels(radius: Int, location: String, context: Context): List<Travel?> {
+        val latLong = AddressTool.getLocationFromAddress(context, location)
+        val tempList =
+            mutableLiveData.value!!.filter { it -> it!!.status == Status.RECEIVED || it.status == Status.SENT }
+        return tempList.filter { it ->
+            latLong?.let { it1 ->
+                AddressTool.getLocationFromAddress(
+                    context, it!!.sourceAdders
+                )?.let { it2 ->
+                    AddressTool.calculateDistance(it1, it2)
                 }
-            }
-        travelDataSource.setNotifyToTravelListListener(notifyToTravelListListener)
+            }!! <= radius
+        }
+    }
+
+    fun updateServiceProvider(travel: Travel) {
+        travel.serviceProvider[FirebaseAuth.getInstance().currentUser?.email] to false
+        travelDataSource.updateTravel(travel)
     }
 }
--- app/src/main/java/com/project/travelmedrivers/utils/Address.kt
+++ app/src/main/java/com/project/travelmedrivers/utils/Address.kt
@@ -1,5 +1,12 @@
 package com.project.travelmedrivers.utils
 
+import android.content.Context
+import android.location.Geocoder
+import com.google.android.gms.maps.model.LatLng;
+import java.io.IOException
+import kotlin.math.*
+
+
 class Address {
     var _city: String
         private set
@@ -19,5 +26,48 @@ class Address {
 
     override fun toString(): String {
         return "Address: $_city, $_street, $_number"
+    }
+}
+
+class AddressTool {
+    companion object {
+        //getLocation From Address
+        fun getLocationFromAddress(context: Context?, strAddress: String?): LatLng? {
+            val coder = Geocoder(context)
+            val address: List<*>?
+            var latLong: LatLng? = null
+            try {
+                // May throw an IOException
+                address = coder.getFromLocationName(strAddress, 5);
+                if (address == null) {
+                    return null
+                }
+                val location = address[0]
+                latLong = LatLng(location.latitude, location.longitude)
+            } catch (ex: IOException) {
+                ex.printStackTrace()
+            }
+            return latLong
+        }
+
+        private const val AVERAGE_RADIUS_OF_EARTH = 6371.0
+        fun calculateDistance(
+            userLatLong: LatLng,
+            venueLatLng: LatLng
+        ): Float {
+            val userLat = userLatLong.latitude;
+            val userLng = userLatLong.longitude;
+            val venueLat = venueLatLng.latitude;
+            val venueLng = venueLatLng.longitude;
+            val latDistance = Math.toRadians(userLat - venueLat)
+            val lngDistance = Math.toRadians(userLng - venueLng)
+            val a = sin(latDistance / 2) * sin(latDistance / 2) +
+                    cos(Math.toRadians(userLat)) *
+                    cos(Math.toRadians(venueLat)) *
+                    sin(lngDistance / 2) *
+                    sin(lngDistance / 2)
+            val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+            return ((AVERAGE_RADIUS_OF_EARTH * c).roundToInt()).toFloat()
+        }
     }
 }
