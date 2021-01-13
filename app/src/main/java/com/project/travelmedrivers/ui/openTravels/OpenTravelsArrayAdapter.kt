ea628731a56c8565d342472742c2693df1440c88
mendi160 <Meni Kuperman@DESKTOP-OERQ00U>
2021-01-13 03:03:36 +0200

    emailintent

--- app/src/main/java/com/project/travelmedrivers/ui/openTravels/OpenTravelsArrayAdapter.kt
+++ app/src/main/java/com/project/travelmedrivers/ui/openTravels/OpenTravelsArrayAdapter.kt
@@ -1,18 +1,25 @@
 package com.project.travelmedrivers.ui.openTravels
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.auth.AuthUI.getApplicationContext
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 import com.project.travelmedrivers.R
 import com.project.travelmedrivers.entities.Travel
 import com.project.travelmedrivers.ui.MainViewModel
+import com.project.travelmedrivers.ui.getPackageName
 import com.project.travelmedrivers.utils.Util
 
 
@@ -51,8 +58,8 @@ class OpenTravelArrayAdapter(
         destination.text = travelList[listPosition]!!.destinationAddress[0]
         date.text = travelList[listPosition]!!.departureDate
         passenger.text = travelList[listPosition]!!.passengers.toString()
-        email.text = travelList[listPosition]!!.email
-        phone.text = "0" + travelList[listPosition]!!.phoneNumber.toString()
+      //  email.text = travelList[listPosition]!!.email
+      //  phone.text = "0" + travelList[listPosition]!!.phoneNumber.toString()
         name.text = travelList[listPosition]!!.name
         if (markerNewTravel.getBoolean(holder.travel.id, false))
             newTravel.visibility = View.VISIBLE
@@ -62,15 +69,17 @@ class OpenTravelArrayAdapter(
             holder.cbIsOfferSent.isChecked = true
     }
 
+    @SuppressLint("RestrictedApi")
     inner class ViewHolder(itemView: View, parent: ViewGroup) : RecyclerView.ViewHolder(itemView),
         View.OnClickListener {
         var source: TextView
         var destination: TextView
         var date: TextView
         var passenger: TextView
-        var email: TextView
-        var phone: TextView
+        var email: FloatingActionButton
+        var phone: FloatingActionButton
         var name: TextView
+        var whatsApp: FloatingActionButton
         var bSendOffer: Button
         var tvNewTravel: TextView
         var cbIsOfferSent: CheckBox
@@ -82,13 +91,24 @@ class OpenTravelArrayAdapter(
             destination = itemView.findViewById(R.id.tvDestination) as TextView
             date = itemView.findViewById(R.id.tvDate) as TextView
             passenger = itemView.findViewById(R.id.tvPassenger)
-            email = itemView.findViewById(R.id.tvEmail)
-            phone = itemView.findViewById(R.id.tvPhoneNumber)
             name = itemView.findViewById(R.id.tvName)
+            email = itemView.findViewById(R.id.bSendEmail)
+            email.setOnClickListener {
 
+                val emailIntent =
+                    Intent(Intent.ACTION_SEND).putExtra(Intent.EXTRA_TEXT, travel.email)
+               // emailIntent.data = Uri.parse("mailto:")
+                emailIntent.type="text/plain"
+                emailIntent.`package`="com.whatsapp"
+                emailIntent.flags=Intent.FLAG_ACTIVITY_NEW_TASK
+
+                getApplicationContext().startActivity(emailIntent)
+
+            }
+            phone = itemView.findViewById(R.id.bPhoneCall)
+            whatsApp = itemView.findViewById(R.id.bWhatsApp)
             bSendOffer = itemView.findViewById(R.id.bSendOffer)
             bSendOffer.setOnClickListener {
-
                 val key = FirebaseAuth.getInstance().currentUser?.email?.let { it1 ->
                     Util.emailToKey(it1)
                 }
@@ -102,32 +122,21 @@ class OpenTravelArrayAdapter(
         }
 
         override fun onClick(v: View?) {
-            if (email.visibility==View.GONE) {
+            if (email.visibility == View.GONE) {
                 email.visibility = View.VISIBLE
                 phone.visibility = View.VISIBLE
+                whatsApp.visibility = View.VISIBLE
                 name.visibility = View.VISIBLE
-                itemView.findViewById<TextView>(R.id.tvEmailLabel).visibility = View.VISIBLE
-                itemView.findViewById<TextView>(R.id.tvPhoneNumberLabel).visibility = View.VISIBLE
-                itemView.findViewById<TextView>(R.id.tvNameLabel).visibility = View.VISIBLE
                 itemView.findViewById<ImageView>(R.id.imageView).rotation = 180F
-            }
-            else  {
-
+            } else {
                 email.visibility = View.GONE
                 phone.visibility = View.GONE
+                whatsApp.visibility = View.GONE
                 name.visibility = View.GONE
-                itemView.findViewById<TextView>(R.id.tvEmailLabel).visibility = View.GONE
-                itemView.findViewById<TextView>(R.id.tvPhoneNumberLabel).visibility = View.GONE
-                itemView.findViewById<TextView>(R.id.tvNameLabel).visibility = View.GONE
                 itemView.findViewById<ImageView>(R.id.imageView).rotation = 0F
             }
-
-
-
         }
-
     }
-
 }
 
 
